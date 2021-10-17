779e99762a90b56ff7729289336ad0b1f0ba4dea
Andrey_Ten <andreiten1@gmail.com>
2021-10-17 22:11:25 +0300

    Test commit

---
+++ Compress_Data/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Compress_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Введите число: ");
+            //string input = Console.ReadLine();
+            //ShannonFano sh = new ShannonFano();
+
+            //sh.Execite(input);
+            //var ssss = sh.Frequencies;
+
+
+            //foreach (KeyValuePair<string, string> entry in myDictionary)
+            //{
+            //    // do something with entry.Value or entry.Key
+            //}
+            //{ }
+            // Останавливаем внутренний таймер объекта Stopwatch
+
+
+            Huffman huffmanTree = new Huffman();
+            double entropy = 0.0d;
+
+            Console.WriteLine("Введите число:");
+            string input = Console.ReadLine();
+            int lenght = input.Length;
+
+            huffmanTree.Execute(input);
+            Stopwatch stopwatch = new Stopwatch();
+            // Запускаем внутренний таймер объекта Stopwatch
+            stopwatch.Start();
+            BitArray encoded = huffmanTree.Encode(input);
+            stopwatch.Stop();
+            Console.WriteLine("Потрачено тактов на выполнение: " + stopwatch.ElapsedTotalMilliseconds);
+            string decoded = huffmanTree.Decode(encoded);
+
+            Console.Write("Кодирование: ");
+
+            foreach (bool bit in encoded)
+            {
+                Console.Write((bit ? 1 : 0) + "");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Декодирование: " + decoded);
+            Console.WriteLine("Алфавит: " + RemoveDuplicates(input));
+            Console.WriteLine("Длина: " + lenght);
+
+            foreach (var symbol in GetFrequencySymbols(input))
+            {
+                double probability = (double)symbol.Value / lenght;
+                entropy -= probability * Math.Log2(probability);
+                Console.WriteLine("Частота символа " + symbol.Key + " - " + symbol.Value + " Вероятность появления: " + probability);
+            }
+
+            Console.WriteLine("Энтропия = " + entropy);
+
+            Console.ReadLine();
+        }
+
+        public static Dictionary<char, int> GetFrequencySymbols(string text)
+        {
+            var result = text
+                .GroupBy(c => c)
+                .ToDictionary(g => g.Key, g => g.Count());
+
+            return result;
+        }
+
+        public static string RemoveDuplicates(string input)
+        {
+            return new string(input.ToCharArray().Distinct().ToArray());
+        }
+
+    }
+}
---
+++ Compress_Data/ShannonFano.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compress_Data
+{
+    class ShannonFano
+    {
+        private List<Node> nodes = new List<Node>();
+        public Node Root { get; set; }
+        public Dictionary<char, int> Frequencies = new Dictionary<char, int>();
+
+        public void Execite(string source)
+        {
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (!Frequencies.ContainsKey(source[i]))
+                {
+                    Frequencies.Add(source[i], 0);
+                }
+
+                Frequencies[source[i]]++;
+            }
+
+            //foreach (KeyValuePair<char, int> symbol in Frequencies)
+            //{
+            //    nodes.Add(new Node() { Symbol = symbol.Key, Frequency = symbol.Value });
+            //}
+
+            //while (nodes.Count > 1)
+            //{
+            //    List<Node> orderedNodes = nodes.OrderBy(node => node.Frequency).ToList();
+
+            //    if (orderedNodes.Count >= 2)
+            //    {
+            //        List<Node> taken = orderedNodes.Take(2).ToList();
+
+            //        Node parent = new Node()
+            //        {
+            //            Symbol = '*',
+            //            Frequency = taken[0].Frequency + taken[1].Frequency,
+            //            Left = taken[0],
+            //            Right = taken[1]
+            //        };
+
+            //        nodes.Remove(taken[0]);
+            //        nodes.Remove(taken[1]);
+            //        nodes.Add(parent);
+            //    }
+
+            //    Root = nodes.FirstOrDefault();
+            //}
+        }
+    }
+}
